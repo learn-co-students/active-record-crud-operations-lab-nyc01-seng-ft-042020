e32ea7d949d888aec2cb595a5f245574470a6158
JeanmarieJackman <jeanmarie.jackman@gmail.com>
2020-05-20 15:41:02 -0400

    Done.

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -29,11 +29,10 @@ def can_be_created_in_a_block(args = { title: "Home Alone", release_date: 1990 }
   # If no arguments are passed, use default values:
   # title == "Home Alone"
   # release_date == 1990
-  m.title = args[:title]
+  Movie.create do |m|
+    m.title = args[:title]
     m.release_date = args[:release_date]
     m.save
-  Movie.create do |m|
-    __
   end
 end
 
@@ -46,7 +45,7 @@ def can_get_the_last_item_in_the_database
 end
 
 def can_get_size_of_the_database
-  Movie.length
+  Movie.count
 end
 
 def can_find_the_first_item_from_the_database_using_id
